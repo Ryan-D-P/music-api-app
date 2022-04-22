bb539367c08a823d9a8924ddf09a89ac974ff751
Lotus 98T <rpitty@live.com>
2022-04-22 23:50:28 +1000

    Musical epochs flex-grid.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import { useEffect, useState } from "react";
+import StDenis from "./images/St.Denis.png";
+import Raphael from "./images/Raphael.jpg";
+import Gallery from "./images/Gallery.jpg";
+import Pannini from "./images/Pannini.png";
+import Moonrise from "./images/Moonrise.jpg";
+import Nouveau from "./images/Nouveau.jpg";
 
 function App() {
+  // Returned classical music API data
   const [musicApiData, setMusicApiData] = useState([]);
+  // Epochs of classical music returned by the music API
+  const epochs = [
+    {name: "Medieval", image: StDenis},
+    {name: "Renaissance", image: Raphael},
+    {name: "Baroque", image: Gallery},
+    {name: "Classical", image: Pannini},
+    {name: "Romantic", image: Moonrise},
+    {name: "20th Century", image: Nouveau},
+  ];
 
   useEffect(() => {
     const getApiData = async () => {
@@ -16,15 +32,16 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1 className="text-4xl text-blue-600">Header text...</h1>
-      <div className="flex flex-wrap justify-center" id="composersWrapper">
+    <div className="App bg-gray-900 min-h-screen">
+      <div className="flex flex-wrap justify-center" id="epochsWrapper">
 
         {
-          musicApiData.map(composer => (
-            <div className="Composer p-6 m-5 bg-blue-400 rounded-lg text-white" key={ composer.id }>
-              <h2 className="text-xl font-bold">{ composer.name }</h2>
-              <img src={ composer.portrait } alt={ `composer-${composer.name}-profile` } />
+          epochs.map(epoch => (
+            <div className="p-6 m-5 bg-gray-800 rounded-lg shadow-lg cursor-pointer transition-transform duration-500 hover:scale-105" key={ epoch.name }>
+              <div className="relative min-w-[480px] min-h-[480px]">
+                <h2 className="text-white text-5xl font-semibold text-center absolute w-full h-full flex justify-center items-center bg-black/50">{ epoch.name }</h2>
+                <img src={ epoch.image } alt={ `${epoch.name}-img` } />
+              </div>
             </div>
           ))
         }

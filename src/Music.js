06c6aaf875604c3b779c2e235bbc25e86f9de126
Lotus 98T <rpitty@live.com>
2022-04-29 00:19:20 +1000

    useFetch custom hook to fetch data.

--- src/Music.js
+++ src/Music.js
@@ -1,34 +1,9 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "./hooks/useFetch";
 
 const Music = () => {
     const { epoch } = useParams();
-    const [musicApiData, setMusicApiData] = useState([]);
-
-    useEffect(() => {
-        const getApiData = async () => {
-            // Docs: https://wiki.openopus.org/wiki/Using_the_Open_Opus_API
-            let response, data;
-
-            if (epoch === `Romantic`) {
-                response = await Promise.all([
-                    fetch(`https://api.openopus.org/composer/list/epoch/Early ${epoch}.json`),
-                    fetch(`https://api.openopus.org/composer/list/epoch/${epoch}.json`),
-                    fetch(`https://api.openopus.org/composer/list/epoch/Late ${epoch}.json`)
-                ]);
-                // Merge all returned composer objects into one composers array
-                data = (await Promise.all(response.map(res => res.json()))).reduce((prev, curr) => [...prev, ...curr.composers], []);
-            }
-            else {
-                response = await fetch(`https://api.openopus.org/composer/list/epoch/${epoch}.json`);
-                data = (await response.json()).composers;
-            }
-
-            setMusicApiData(data);
-        };
-
-        getApiData();
-    }, []);
+    const [musicApiData] = useFetch(epoch);
 
     return (
         <div className="Music">
